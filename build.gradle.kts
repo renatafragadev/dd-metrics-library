dbef04d7801c7d64d0736a9ab691c53d47f54075
Renata Fraga <renatafragadev@gmail.com>
2023-05-10 22:58:49 -0300

    adding modules (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,15 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-	id("org.springframework.boot") version "3.0.6"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+	kotlin("jvm") apply false
+	kotlin("multiplatform") apply false
+	kotlin("plugin.spring") apply false
+	id("org.jetbrains.dokka") apply false
+	id("org.jmailen.kotlinter") apply false
 }
 
-group = "edu.renatafraga"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
-repositories {
-	mavenCentral()
+allprojects {
+	version = "0.0.1"
 }
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
+subprojects {
+	group = "edu.renatafraga"
 }
